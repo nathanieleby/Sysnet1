9ae7aca9a9ad4f0af1764de1097cf5ad0de27785
116437 <nathan.moore@sncorp.com>
2023-05-25 14:26:37 -0600

    Indentation inconsistancy fixed.

--- src/FileSystemP5/main.cpp
+++ src/FileSystemP5/main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int main()
+int main(int argc, char **argv)
 {
 
-cout  << "Hello World!" << endl
+  cout  << "Hello World!" << endl;
   
+  if(argc>0)
+  {
+    cout << argv << endl;
+  }
+
   return 0;
 }
